4ddd40d53c601227d21a3908039d16b0d6b2c129
Patricia Miller <patriciamill18@gmail.com>
2021-01-19 13:53:44 -0700

    move to new folder

--- myfile.R
+++ myfile.R
@@ -11,3 +11,8 @@ ggplot(data=mpg, aes(x=hwy, fill=drv)) + geom_bar()
 ## make some edits ##
 
 ggplot(data=mpg, aes(x=hwy, fill=class)) + geom_bar()
+
+## try to edit this and see if it works
+
+print(paste("The mean of cyl is ", round(mean(mpg$cyl), 2), ".", sep = ""))
+
